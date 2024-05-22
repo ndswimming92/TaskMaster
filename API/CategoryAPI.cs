2e680fe08ee3bcc923320b5c1ffea5b6b5ff3b50
Nicholas Davidson <nicholas.davidson.20@gmail.com>
2024-05-21 19:14:57 -0500

    Get All Categories and single Category endpoints.

--- API/CategoryAPI.cs
+++ API/CategoryAPI.cs
@@ -1,6 +1,30 @@
-﻿namespace TaskMaster.API
+﻿using TaskMaster.Models;
+
+namespace TaskMaster.API
 {
     public class CategoryAPI
     {
+        public static void Map(WebApplication app)
+        {  
+            // Get All Categories
+            app.MapGet("/api/category", (TaskMasterDbContext db) =>
+            {
+                return db.Categories.ToList();
+            });
+
+            // Get Single Category
+            app.MapGet("/api/category/{id}", (TaskMasterDbContext db, int id) =>
+            {
+                var itemID = db.Categories.FirstOrDefault(c => c.Id == id);
+
+                if (itemID == null)
+                {
+                    return Results.NotFound("Category Not Found.");
+                }
+
+                return Results.Ok(itemID);
+            });
+
+        }
     }
 }
