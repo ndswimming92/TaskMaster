ac55e6e492ccbf7c5546fe95ca284c2f8cd5717e
Nicholas Davidson <nicholas.davidson.20@gmail.com>
2024-05-21 21:14:57 -0500

    Created Update User API

--- API/UserAPI.cs
+++ API/UserAPI.cs
@@ -15,7 +15,23 @@ namespace TaskMaster.API
 
             });
 
-            
+            // Update a User API
+
+            app.MapPut("api/user/{id}", (TaskMasterDbContext db, int id, User users) =>
+            {
+                DateTime modifyDate = DateTime.Now;
+                User updateUser = db.Users.SingleOrDefault(users => users.Id == id);
+                if (updateUser == null)
+                {
+                    return Results.NotFound("User not found");
+                }
+                updateUser.Id = users.Id;
+                updateUser.UserName = users.UserName;
+                updateUser.Password = users.Password;
+                
+                db.SaveChanges();
+                return Results.Ok(users);
+            });
 
         }
 
