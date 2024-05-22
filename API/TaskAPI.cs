7ec39c8994406344183fe47b1498860070c32237
Nicholas Davidson <nicholas.davidson.20@gmail.com>
2024-05-21 19:36:56 -0500

    Get Task by Id

--- API/TaskAPI.cs
+++ API/TaskAPI.cs
@@ -12,7 +12,18 @@ namespace TaskMaster.API
                 return db.Tasks.ToList();
             });
 
-            
+            // Get Tasks by Id
+            app.MapGet("/api/task/{id}", (TaskMasterDbContext db, int id) =>
+            {
+                var task = db.Tasks.FirstOrDefault(task => task.Id == id);
+
+                if (task == null)
+                {
+                    return Results.NotFound("Task was not found.");
+                }
+
+                return Results.Ok(task);
+            });
         }
     }
 }
